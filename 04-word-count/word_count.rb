eb8c3505f79d7f918686be8c1f43eb88d697a7cc
Cheeron Mars <cheeronmars+gh@posteo.org>
2018-04-07 16:55:32 -0700

    created word count method

--- 04-word-count/word_count.rb
+++ 04-word-count/word_count.rb
@@ -1,1 +1,14 @@
-
+def words(sentence)
+  word_counts = {}
+  array_sentence = sentence.split(" ")
+  array_sentence.each do |i|
+    if i != ""
+      if word_counts[i] == nil
+        word_counts[i] = 1
+      else
+        word_counts[i] += 1
+      end
+    end
+  end
+  return word_counts
+end
